ad1cd4595d3ac622186c45e0ccfa77d5bca9a7c5
shub-h7777 <Shubham.Patel@einfochips.com>
2023-03-28 13:11:18 +0530

    swag_labs_add_to_cart

--- z_assignment/swag_labs_ios.py
+++ z_assignment/swag_labs_ios.py
@@ -1,3 +1,5 @@
+import time
+
 import pytest
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
@@ -10,14 +12,14 @@ class AppiumConfig():
     def launch_app(self):
         des_cap = {
             # Set URL of the application under test
-            "app": "bs://043b78b811594e7ccc9cce0ff27c94788ba47c6e",
+            "app": "bs://ce349708e017ea8d0a05c59c5f86402af60f66d7",
             # Specify device and os_version for testing
             "deviceName": "iPhone 11 Pro",
             "platformVersion": "13.1",
             # Set other BrowserStack capabilities
             "bstack:options": {
-                "userName": "shubham_IABeCR",
-                "accessKey": "Mppd3EzBym4j9FPUq8ui",
+                "userName": "shubham_vMsZey",
+                "accessKey": "PfytQFzr4F2xnm99CQfK",
                 "projectName": "First Python project",
                 "buildName": "browserstack-build-1",
                 "sessionName": "BStack first_test"
@@ -25,7 +27,7 @@ class AppiumConfig():
         }
 
         self.driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", desired_capabilities=des_cap)
-        self.driver.implicitly_wait(5)
+        self.driver.implicitly_wait(10)
         yield
         self.driver.quit()
 
@@ -39,3 +41,36 @@ class Test_Swag_Labs(AppiumConfig):
                                               '//XCUIElementTypeStaticText[@name="Username and password do not match any user in this service."]').text
         print(error_text)
         assert_that("Username and password do not match any user in this service.").is_equal_to(error_text)
+
+    def test_add_to_cart(self):
+        self.driver.find_element(AppiumBy.NAME, "test-Username").send_keys("standard_user")
+        self.driver.find_element(AppiumBy.NAME, "test-Password").send_keys("secret_sauce")
+        self.driver.find_element(AppiumBy.NAME, "test-LOGIN").click()
+        self.driver.find_element(AppiumBy.XPATH, '(//XCUIElementTypeOther[@name="test-ADD TO CART"])').click()
+        self.driver.find_element(AppiumBy.XPATH, '(//XCUIElementTypeOther[@name="test-ADD TO CART"])').click()
+        self.driver.find_element(AppiumBy.XPATH, '(//XCUIElementTypeOther[@name="test-ADD TO CART"])').click()
+        self.driver.find_element(AppiumBy.XPATH, '(//XCUIElementTypeOther[@name="test-ADD TO CART"])').click()
+
+
+        # click on cart
+        self.driver.find_element(AppiumBy.XPATH, '//XCUIElementTypeOther[@name="test-Cart"]').click()
+
+        #scroll to checkout
+        para_dic={"direction": "down", "predicateString": "name=='test-CHECKOUT'", "toVisible": True}
+        self.driver.execute_script("mobile: scroll", para_dic)
+
+
+        self.driver.find_element(AppiumBy.NAME, "test-CHECKOUT").click()
+        self.driver.find_element(AppiumBy.NAME, "test-First Name").send_keys("Shubham")
+        self.driver.find_element(AppiumBy.NAME, "test-Last Name").send_keys("Patel")
+        self.driver.find_element(AppiumBy.NAME, "test-Zip/Postal Code").send_keys("400001")
+        self.driver.find_element(AppiumBy.NAME, "Return").click()
+
+        self.driver.find_element(AppiumBy.NAME,"test-CONTINUE").click()
+
+
+
+        card_details=self.driver.find_element(AppiumBy.XPATH,'//XCUIElementTypeStaticText[contains(@name,"Payment Information")]').text
+        print(card_details)
+
+
